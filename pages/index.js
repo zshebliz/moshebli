27cfdfd45252186a42f5371da0d7b19866f297c1
zshebliz <116478328+zshebliz@users.noreply.github.com>
2023-02-14 20:08:15 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Mohammad Shebli" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>الموقع قيد التطوير حاليا</code>
         </p>
       </main>
 
